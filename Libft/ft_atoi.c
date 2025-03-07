d3c6691da31d82f37943203c42a5e3163c01f78a
Ali Bouknana <abouknan@e2r11p5.1337.ma>
2025-03-07 03:29:49 +0000

    a

---
+++ Libft/ft_atoi.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouknan <abouknan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 03:11:51 by abouknan          #+#    #+#             */
+/*   Updated: 2025/03/05 09:49:17 by abouknan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	mul;
+	int	i;
+	int	result;
+
+	mul = 1;
+	i = 0;
+	result = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+	{
+		mul = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	if (str[i] != '\0')
+		return (0);
+	return (result * mul);
+}
